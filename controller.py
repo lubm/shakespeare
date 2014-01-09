3d20c0d761a6da21ef5525f074964378c7a4f2e5
carohadad <carohadad@carohadad-macbookpro.roam.corp.google.com>
2014-01-09 10:56:44 -0800

    added homepage with google look and feel

--- controller.py
+++ controller.py
@@ -2,22 +2,22 @@ import webapp2
 import cgi
 from google.appengine.ext.webapp import template
 
-
 class MainPage(webapp2.RequestHandler):
 
     def get(self):
+    	searched_value = self.response.write(cgi.escape(self.request.get('value')))
+
         self.response.headers['Content-Type'] = 'text/html'
-        self.response.write(template.render('index.html', {}))
+        self.response.write(template.render('index.html', {'value' : searched_value}))
 
-class SearchResponse(webapp2.RequestHandler):
 
-    def post(self):
-    	self.response.write('<html><body>You wrote:<pre>')
-        self.response.write(cgi.escape(self.request.get('value')))
-        self.response.write('</pre></body></html>')
+class HomePage(webapp2.RequestHandler):
+
+	def get(self):
+		self.response.headers['Content-Type'] = 'text/html'
+		self.response.write(template.render('homepage.html', {}))
 
 
 application = webapp2.WSGIApplication([
-    ('/', MainPage),
-    ('/search', SearchResponse),
+    ('/', HomePage),
 ], debug=True)
