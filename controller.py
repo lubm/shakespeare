812167d31b19410e7d66f7faacfdc250cd55b3e3
carohadad <carohadad@carohadad-macbookpro.roam.corp.google.com>
2014-01-09 11:51:27 -0800

    Using a template to send values to the view

--- controller.py
+++ controller.py
@@ -1,21 +1,26 @@
 import webapp2
-import cgi
 from google.appengine.ext.webapp import template
 
 class MainPage(webapp2.RequestHandler):
 
     def get(self):
-    	searched_value = self.response.write(cgi.escape(self.request.get('value')))
+    	searched_value = self.request.get('searched_word')
+
+    	value = searched_value if searched_value is not None else ''
+
+    	template_values = {
+            'searched_word': value,
+        }
 
         self.response.headers['Content-Type'] = 'text/html'
-        self.response.write(template.render('index.html', {'value' : searched_value}))
+        self.response.out.write(template.render('index.html', template_values))
 
 
 class HomePage(webapp2.RequestHandler):
 
 	def get(self):
 		self.response.headers['Content-Type'] = 'text/html'
-		self.response.write(template.render('homepage.html', {}))
+		self.response.out.write(template.render('homepage.html', {}))
 
 
 application = webapp2.WSGIApplication([
