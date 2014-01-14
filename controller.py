651afdd276963f3a3b8b921a2112fe3ab24c9087
Luciana Bicalho Maroun <maroun@google.com>
2014-01-13 18:07:08 -0800

    Grouped results by work and changed UI

--- controller.py
+++ controller.py
@@ -1,4 +1,5 @@
 import cgi
+import re
 
 import webapp2
 from google.appengine.ext.webapp import template
@@ -14,17 +15,29 @@ class MainPage(webapp2.RequestHandler):
 
     	value = searched_value if searched_value else ''
 
-        mentions = []
+        work_mentions = []
         if value:
             word = Word.get_from_index(cgi.escape(value))
             if word:
-                mentions = word.mentions
+                # Grouping mentions by work for UI display
+                work_mentions = {}               
+                for mention in word.mentions:
+                    # Making the words stay bold
+                    line = re.sub(value, "<b>%s</b>" % value, mention.line)
+                    if mention.work not in work_mentions:
+                        work_mentions[mention.work] = []
+                    work_mentions[mention.work].append(line)
+
+        print work_mentions
+        if work_mentions:
+            print "------not empty"
+        for work in work_mentions:
+            print work
 
     	template_values = {
             'searched_word': value,
-            'mentions_list': mentions
+            'work_mentions': work_mentions
         }
-        print mentions
 
         self.response.headers['Content-Type'] = 'text/html'
         self.response.out.write(template.render('index.html', template_values))
