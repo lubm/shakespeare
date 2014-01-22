8a42bb7900515883c45359dc338510c3d75564d4
carohadad <carohadad@carohadad-macbookpro.roam.corp.google.com>
2014-01-22 14:32:31 -0800

    definition service

--- auxiliaries/definition_service.py
+++ auxiliaries/definition_service.py
@@ -1,16 +1,52 @@
 import json
 import urllib2
+from protorpc import remote
+from protorpc import messages
+from protorpc.transport import HttpTransport
 
-class DefinitionService(object):
-    """Definition Ser
+class DefinitionRequest(messages.Message):
+    """Request object used to communicate with the DefinitionService.
+
+    Attributes:
+        term: contains word to be searched. This attribute is required.
+    """
+    term = messages.StringField(1, required=True)
+
+class DefinitionResponse(messages.Message):
+    """Response object returned by the DefinitionService.
+
+    Attributes:
+        definition: list of
+    """
+    definition = messages.StringField(1, repeated=True)
+
+
+class DefinitionService(remote.Service):
+    """Remote Server used to define words.
+
+    Attributes:
+        service: Definition services address. 
     """
     def __init__(self):
-        self.service_url = "http://definition-server.appspot.com/definition.define"
+        """DefinitionService initialization.
 
-    def define(self, searched_value):
-        values = {
-            'term' : searched_value,
-        }
-        req = urllib2.Request(self.service_url)
-        req.add_header('Content-Type', 'application/json')
-        return urllib2.urlopen(req, json.dumps(values)).read()
+        Assigns the service attribute to link to the service url.
+        """
+        service_url = 'http://definition-server.appspot.com/definition'
+        self.service = self.Stub(HttpTransport(service_url))
+
+
+    @remote.method(DefinitionRequest, DefinitionResponse)
+    def define(self, request):
+        """Performs an RPC call to the definition service to obtain the first 
+        definiton of the word.
+
+        Args:
+            request: DefinitionRequest object containing the word to be defined 
+            by the definition service.
+
+        Returns:
+            A DefinitionResponse object containing a list of definitions for the
+            searched word.
+        """
+        return self.service.define(request)
--- define_page_controller.py
+++ define_page_controller.py
@@ -1,6 +1,6 @@
 import webapp2
 from auxiliaries.definition_service import DefinitionService
-
+from auxiliaries.definition_service import DefinitionRequest
 
 class DefinePageController(webapp2.RequestHandler):
     """Handler for Definition features.
@@ -17,7 +17,12 @@ class DefinePageController(webapp2.RequestHandler):
         Gets the searched word from the request and calls the DefinitionService
         in order to obtain its definion. Returns the result as plain text.
         """
-        searched_value = self.request.get('searched_word')
-        word_definition = DefinitionService().define(searched_value)
+        definition_request = DefinitionRequest()
+        definition_request.term = self.request.get('searched_word')
+
+        definition_service = DefinitionService()
+        definition_response = definition_service.define(definition_request)
+        word_first_definition = definition_response.definition[0]
+
         self.response.headers['Content-Type'] = 'text/plain'
-        self.response.out.write(word_definition)
+        self.response.out.write(word_first_definition)
