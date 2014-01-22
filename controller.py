bf221f8a26df7d683ce13435705fac246009e296
carohadad <carohadad@carohadad-macbookpro.roam.corp.google.com>
2014-01-22 09:51:41 -0800

    Fixed pylint errors in controller.py

--- controller.py
+++ controller.py
@@ -1,3 +1,7 @@
+""" Routes for each group of actions with their handlers."""
+
+# F:  2, 0: Unable to import 'webapp2'
+# pylint: disable=F0401
 import webapp2
 
 from admin_page_controller import AdminPageController
@@ -7,7 +11,6 @@ from home_page_controller import HomePageController
 from main_page_controller import MainPageController
 from define_page_controller import DefinePageController
 
-        
 application = webapp2.WSGIApplication([
     ('/define', DefinePageController),
     ('/', HomePageController),
