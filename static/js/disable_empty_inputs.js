35474299765ed607844848a9d7687d2e43ab626b
carohadad <carohadad@carohadad-macbookpro.roam.corp.google.com>
2014-01-27 10:43:20 -0800

    Fixed lint warnuings in the js file

--- static/js/disable_empty_inputs.js
+++ static/js/disable_empty_inputs.js
@@ -2,15 +2,15 @@ $(document).ready(function(){
 	var disable = true;
 	if ($('#search-value').val().length > 0) {
 		disable = false;
-	};
+	}
     $('#search-button').attr('disabled', disable);
-    
+
     $('#search-value').keyup(function(){
         if($(this).val().length > 0){
-            disabled = false
+            disabled = false;
         } else {
-        	disabled = true
+        	disabled = true;
         }
        	$('#search-button').attr('disabled', disabled);
-    })
+    });
 });
