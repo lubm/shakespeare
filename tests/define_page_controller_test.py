912f8f8e2aec4ea7b546053ddc286d7347dfbf29
carohadad <carohadad@carohadad-macbookpro.roam.corp.google.com>
2014-02-03 10:24:34 -0800

    fixed pylint warnings in  define_page_controller_test

--- tests/define_page_controller_test.py
+++ tests/define_page_controller_test.py
@@ -1,11 +1,16 @@
 import webtest
 from controllers.define_page import DefinePageController
-from google.appengine.ext import testbed
 import unittest
 import  webapp2
 
+"""
+Run this tests like this:
+nosetests --with-gae tests/define_page_controller_test.py
+"""
+
 class DefinePageControllerTest(unittest.TestCase):
     """Tests for the handler of the definition service"""
+
     def setUp(self):
         """Create the dummy webapp"""
         app = webapp2.WSGIApplication([('/define', DefinePageController)])
@@ -15,14 +20,14 @@ class DefinePageControllerTest(unittest.TestCase):
         """Search a word that is found"""
         response = self.testapp.get('/define?searched_word=Friend')
         self.assertEqual(response.status_int, 200)
-        self.assertEqual(response.normal_body, 
+        self.assertEqual(response.normal_body,
             'a person you know well and regard with affection and trust')
         self.assertEqual(response.content_type, 'text/plain')
 
-    def test_get_definition_of_an_invalid_word(self):
+    def test_define_invalid_word(self):
         """Search a word that is not found"""
         response = self.testapp.get('/define?searched_word=asts')
         self.assertEqual(response.status_int, 200)
-        self.assertEqual(response.normal_body, 
+        self.assertEqual(response.normal_body,
             'Word not found in the dictionary')
         self.assertEqual(response.content_type, 'text/plain')
