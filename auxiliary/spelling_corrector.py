fc8fb0e791b9c74d99365062b8c6b97312bca88d
carohadad <carohadad@carohadad-macbookpro.roam.corp.google.com>
2014-02-05 14:04:17 -0800

    More testing and refactoring

--- auxiliary/spelling_corrector.py
+++ auxiliary/spelling_corrector.py
@@ -4,27 +4,56 @@ import collections
 from models.word import Word
 
 class SpellingCorrector(object):
-  def __init__(self):
-    self.alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    def __init__(self):
+        self.alphabet = 'abcdefghijklmnopqrstuvwxyz'
 
-  def edits1(self, word):
-    splits     = [(word[:i], word[i:]) for i in range(len(word) + 1)]
-    deletes    = [a + b[1:] for a, b in splits if b]
-    transposes = [a + b[1] + b[0] + b[2:] for a, b in splits if len(b)>1]
-    replaces   = [a + c + b[1:] for a, b in splits for c in self.alphabet if b]
-    inserts    = [a + c + b     for a, b in splits for c in self.alphabet]
-    return set(deletes + transposes + replaces + inserts)
 
-  def known_edits2(self, word):
-    return set(e2 for e1 in self.edits1(word) for e2 in self.edits1(e1) if e2 in Word.all())
+    def splits(self, word):
+        return [(word[:split_index], word[split_index:]) 
+            for split_index in range(len(word) + 1)]
 
-  def known_words(self, words): 
-    known_words = []
-    for word in words:
-      if Word.get_by_id(word):
-        known_words.append(word)
-    return known_words
 
-  def correct(self, word):
-    candidates = self.known_words([word]) or self.known_words(self.edits1(word)) or self.known_edits2(word) or [word]
-    return max(candidates, key=Word.all())
+    def deletes(self, list_of_splited_words):
+        return [first_part + second_part[1:] 
+            for first_part, second_part in list_of_splited_words if second_part]
+
+    def transposes(self, list_of_splited_words):
+        transposes = []
+        for first_part, second_part in list_of_splited_words:
+            if len(second_part) > 1:
+                new_element = first_part + second_part[1] + second_part[0] + second_part[2:]
+                transposes.append(new_element)
+        return transposes
+
+    def replaces(self, list_of_splited_words):
+        return [a + c + b[1:] 
+            for a, b in list_of_splited_words for c in self.alphabet if b]
+
+    def inserts(self, list_of_splited_words):
+        return [a + c + b
+            for a, b in list_of_splited_words for c in self.alphabet]
+            
+
+    def words_edit_distance_one(self, word):
+        splits = self.splits(word)
+
+        deletes = self.deletes(splits)
+        transposes = self.transposes(splits)
+        replaces = self.replaces(splits)
+        inserts = self.inserts(splits)
+
+        return set(deletes + transposes + replaces + inserts)
+
+    def words_edit_distance_two(self, word):
+        return set(e2 for e1 in self.words_edit_distance_one(word) for e2 in self.words_edit_distance_one(e1) if e2 in Word.all())
+
+    def known_words(self, words): 
+        known_words = []
+        for word in words:
+          if Word.get_by_id(word):
+            known_words.append(word)
+        return known_words
+
+    def correct(self, word):
+        candidates = self.known_words([word]) or self.known_words(self.words_edit_distance_one(word)) or self.words_edit_distance_two(word) or [word]
+        return max(candidates, key=Word.all())
--- tests/spelling_corrector_test.py
+++ tests/spelling_corrector_test.py
@@ -1,4 +1,5 @@
 import unittest
+import random
 
 from google.appengine.ext import ndb
 from google.appengine.ext import testbed
@@ -39,5 +40,67 @@ class SpellingCorrectorTest(unittest.TestCase):
 
         self.testbed.deactivate()
 
-    def test_correct_a_word(self):
-        self.assertEqual(self.spelling_corrector.edits1('lave'), [])
+    def test_generate_splits_of_a_word(self):
+        self.assertEqual(self.spelling_corrector.splits('love'), 
+            [('', 'love'), ('l', 'ove'), ('lo', 've'), ('lov', 'e'), 
+            ('love', '')])
+
+    def test_generate_deletes_of_a_list_of_splited_words(self):
+        self.assertEqual(self.spelling_corrector.deletes(
+            [('lo', 've'), ('l', 'ove')]), 
+            ['loe', 'lve'])
+
+    def test_generate_transposes_of_a_list_of_splited_words(self):
+        self.assertEqual(self.spelling_corrector.transposes(
+            [('lo', 've'), ('l', 'ove')]), 
+            ['loev', 'lvoe'])
+
+    def test_generate_replaces_of_a_list_of_splited_words(self):
+        replaces = []
+        for letter in 'abcdefghijklmnopqrstuvwxyz':
+            replaces.append('lo' + letter + 'e')
+        for letter in 'abcdefghijklmnopqrstuvwxyz':
+            replaces.append('l' + letter + 've')
+        self.assertEqual(self.spelling_corrector.replaces(
+            [('lo', 've'), ('l', 'ove')]), replaces)
+
+    def test_generate_inserts_of_a_list_of_splited_words(self):
+        inserts = []
+        for letter in 'abcdefghijklmnopqrstuvwxyz':
+            inserts.append('lo' + letter + 've')
+        for letter in 'abcdefghijklmnopqrstuvwxyz':
+            inserts.append('l' + letter + 'ove')
+        self.assertEqual(self.spelling_corrector.inserts(
+            [('lo', 've'), ('l', 'ove')]), inserts)
+
+    def test_words_edit_distance_one_should_contain_all_the_types(self):
+        option1 = random.choice(
+            self.spelling_corrector.deletes([('lo', 've'), ('l', 'ove')]))
+
+        option2 = random.choice(
+            self.spelling_corrector.replaces([('lo', 've'), ('l', 'ove')]))
+
+        option3 = random.choice(
+            self.spelling_corrector.transposes([('lo', 've'), ('l', 'ove')]))
+
+        option4 = random.choice(
+            self.spelling_corrector.inserts([('lo', 've'), ('l', 'ove')]))
+
+        self.assertTrue(option1 in 
+            self.spelling_corrector.words_edit_distance_one('love'))
+
+        self.assertTrue(option2 in 
+            self.spelling_corrector.words_edit_distance_one('love'))
+
+        self.assertTrue(option3 in 
+            self.spelling_corrector.words_edit_distance_one('love'))
+
+        self.assertTrue(option4 in 
+            self.spelling_corrector.words_edit_distance_one('love'))
+
+
+    def test_words_edit_distance_two(self):
+        Word(id="borwer", name="borwer", count=4).put()
+        
+        self.assertTrue('borwer' 
+            in self.spelling_corrector.words_edit_distance_two('borrower'))
