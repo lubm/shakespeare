cd026b9f7c4a837c26752f9f6c6e7cf38e7d0b6a
carohadad <carohadad@carohadad-macbookpro.roam.corp.google.com>
2014-02-06 15:17:56 -0800

    Adding tests for character model

--- models/word.py
+++ models/word.py
@@ -1,14 +1,14 @@
-"""Module for the model of a word"""
+'''Module for the model of a word.'''
 from google.appengine.ext import ndb
 
 class Word(ndb.Model):
-    """Models a word. The relation to mentions is
+    '''Models a word. The relation to mentions is
        represented by MentionsInWork objects, which has the key of a Word object
        as parent.
 
     Attributes:
-    	name: the name of the word (or the word itself)
-    	count: the amount of times the word appears in all the works.
-    """
+        name: the name of the word (or the word itself)
+        count: the amount of times the word appears in all the works.
+    '''
     name = ndb.StringProperty()
     count = ndb.IntegerProperty()
