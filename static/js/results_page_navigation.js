cc59e56c2e0cfd2c38a752bc5a335c8d60130a90
carohadad <carohadad@carohadad-macbookpro.roam.corp.google.com>
2014-02-07 12:47:29 -0800

    increased the size of the treemap

--- static/js/results_page_navigation.js
+++ static/js/results_page_navigation.js
@@ -1,16 +1,16 @@
 $(window).load(function() {
-    $('#visualization-tab-content').hide();
+    $('#visualization-tab-content').css('visibility', 'hidden');
     
     $('#show-visualization').click(function() {
-        $('#result-tab-content').hide();
-        $('#visualization-tab-content').show();
+        $('#result-tab-content').css('visibility', 'hidden');
+        $('#visualization-tab-content').css('visibility', 'visible');
         $('#show-visualization').parent().addClass('active');
         $('#show-results').parent().removeClass('active');
     });
 
     $('#show-results').click(function() {
-        $('#visualization-tab-content').hide();
-        $('#result-tab-content').show();
+        $('#visualization-tab-content').css('visibility', 'hidden');
+        $('#result-tab-content').css('visibility', 'visible');
         $('#show-results').parent().addClass('active');
         $('#show-visualization').parent().removeClass('active');
     });
