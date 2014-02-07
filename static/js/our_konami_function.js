175b8f8f11e0399f41c7aaef1dd2a53cfa8607e9
carohadad <carohadad@carohadad-macbookpro.roam.corp.google.com>
2014-02-06 21:49:43 -0800

    Finished shakespeare animation

--- static/js/our_konami_function.js
+++ static/js/our_konami_function.js
@@ -1,8 +1,13 @@
 $(window).load(function() {
-	var easter_egg = new Konami(function() { 
-		$(document).ready(function() {
-    		$("#shakespeare-image").animate({left: "+=500"}, 9000);
-    		$("#shakespeare-image").animate({left: "-=300"}, 9000);
-		});
-	})
+    $("#shakespeare-image").css('display', 'none');
+    $("#cs-academy").css('display', 'none');
+    var easter_egg = new Konami(function() { 
+        $("#shakespeare-image").css('display', 'block');
+        $("#shakespeare-image").animate({left: "+=500"}, 1000);
+        $("#cs-academy").delay(1000).fadeIn(300);
+        $("#shakespeare-image").animate({left: "+=300"}, 500);
+        $("#cs-academy").delay(1500).fadeOut(300);
+        $("#shakespeare-image").animate({left: "-=800"}, 1000);
+        $("#shakespeare-image").delay(3000).fadeOut(300);
+    })
 });
