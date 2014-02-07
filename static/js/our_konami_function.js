cb7561f467b955baae30bc86b62f95054bab406a
carohadad <carohadad@carohadad-macbookpro.roam.corp.google.com>
2014-02-06 18:50:39 -0800

    work in progress changing konami

--- static/js/our_konami_function.js
+++ static/js/our_konami_function.js
@@ -1,5 +1,8 @@
 $(window).load(function() {
 	var easter_egg = new Konami(function() { 
-		alert('CS Academy! :) :)');
+		$(document).ready(function() {
+    		$("#shakespeare-image").animate({left: "+=500"}, 9000);
+    		$("#shakespeare-image").animate({left: "-=300"}, 9000);
+		});
 	})
 });
