bf07715f5ffaaff10864ab1b91b867f8003c9bba
carohadad <carohadad@carohadad-macbookpro.roam.corp.google.com>
2014-02-06 21:58:38 -0800

    changed timing in konami for first page

--- static/js/our_konami_function.js
+++ static/js/our_konami_function.js
@@ -5,9 +5,9 @@ $(window).load(function() {
         $("#shakespeare-image").css('display', 'block');
         $("#shakespeare-image").animate({left: "+=500"}, 1000);
         $("#cs-academy").delay(1000).fadeIn(300);
-        $("#shakespeare-image").animate({left: "+=300"}, 500);
-        $("#cs-academy").delay(1500).fadeOut(300);
+        $("#shakespeare-image").animate({left: "+=200"}, 500);
         $("#shakespeare-image").animate({left: "-=800"}, 1000);
-        $("#shakespeare-image").delay(3000).fadeOut(300);
+        $("#shakespeare-image").delay(1500).fadeOut(300);
+        $("#cs-academy").delay(3500).fadeOut(600);
     })
 });
