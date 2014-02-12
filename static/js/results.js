34d28be79cf4debc21ea680a9fdc01b27cf591e3
maroun <maroun@maroun-macbookpro.roam.corp.google.com>
2014-02-12 00:38:39 -0800

    Fixed merge bugs.

--- static/js/results.js
+++ static/js/results.js
@@ -57,15 +57,10 @@ function insertResultsInfo(time, number_results) {
 
 function insertDidYouMean(did_you_mean) {
     if (did_you_mean) {
-        message = document.createElement('span');
-        $(message).text('Did you mean: ');
-        $(message).addClass('google-red');
-        suggestion = document.createElement('a');
-        suggestion.href = '/results?searched_word=' + did_you_mean;
-        $(suggestion).text(did_you_mean);
-        $(suggestion).css('font-style', 'bold italic');
-        $('#did-you-mean').append(message);
-        $('#did-you-mean').append(suggestion);
+        $('#did-you-mean-sugg').attr('href', '/results?searched_word=' + 
+            did_you_mean);
+        $('#did-you-mean-sugg').text(did_you_mean);
+        $('#did-you-mean').show();
     }
 }
 
